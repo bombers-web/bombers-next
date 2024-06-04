ccf90eec7c8da59136713a2549529d0fe315a263
pablo <pa.romerorgz@gmail.com>
2024-06-04 03:06:31 -0400

    error handling

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,12 +7,11 @@ export function getStrapiURL(path = "", useLocal) {
 // Helper to make GET requests to Strapi
 export async function fetchAPI(path, useLocal = false) {
   const requestUrl = getStrapiURL(path, useLocal);
-  console.log({ requestUrl });
   try {
     const response = await fetch(requestUrl);
     return await response?.json();
   } catch (error) {
     console.error("We're offline");
-    return {};
+    throw new Error(error);
   }
 }
