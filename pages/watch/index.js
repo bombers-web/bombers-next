4c81ef1f6aa046da167c9472b799e6f448eb5416
edavidson1410 <edavidson1410@gmail.com>
2024-09-18 17:02:03 -0500

    Added index layout to watch page

--- pages/watch/index.js
+++ pages/watch/index.js
@@ -1,7 +1,36 @@
 import Layout from "src/common/Layout";
+import { Link, Box, SimpleGrid } from "@chakra-ui/react";
+import IndexLayout from "components/IndexLayout";
 
 const Watch = () => {
-  return <Layout>WatchPage TBD</Layout>;
+  const streamingSources = [
+    {
+      name: "Ahlmeyer Sports Productions",
+      href: "https://www.youtube.com/@asp-ahlemeyersportsproduct192/featured",
+    },
+    {
+      name: "Bombers Youtube",
+      href: "https://www.youtube.com/channel/UCHgkyW1v70rioXkrQJVdMWQ",
+    },
+    {
+      name: "Bombers VEO",
+      href: "https://veolive.page.link/R72N",
+    },
+  ];
+
+  return (
+    <>
+      <Layout
+        header="Watch Info"
+        seo={{ metaTitle: "bombers watch information" }}
+        margin
+      >
+        <Box h="100vh">
+          <IndexLayout items={streamingSources} />;
+        </Box>
+      </Layout>
+    </>
+  );
 };
 
 export default Watch;
