1972dbbfd22f170e651d872d113bdcf303a1ba26
edavidson1410 <edavidson1410@gmail.com>
2024-10-20 13:06:16 -0500

    replaced team page to add index layout

--- pages/club/index.tsx
+++ pages/club/index.tsx
@@ -4,44 +4,14 @@ import useNav from "hooks/useNav";
 
 const ClubIndex = () => {
   const { navs } = useNav();
-  console.log(navs)
-  // TODO: reestablish useNav
-  // const subMenus = navs[0].subMenus;
-
-  // //temporary
-  // const subMenus = [
-  //   {
-  //     name: "history",
-  //     id: "history",
-  //     slug: "club/history",
-  //     bg: "/static/legends_3.jpeg",
-  //   },
-  //   {
-  //     name: "board",
-  //     id: "board",
-  //     slug: "club/board",
-  //     bg: "",
-  //   },
-  //   {
-  //     name: "youth rugby",
-  //     id: "youth-rugby",
-  //     slug: "club/youth-rugby",
-  //     bg: "/static/jets_mark.jpg",
-  //   },
-    // {
-    //   name: "bombers career center",
-    //   id: "youth-rugby",
-    //   slug: "club/career-center",
-    //   bg: "",
-    // },
-  // ];
+  const clubSubNav = navs[2].subMenus;
 
   return (
     <Layout
       header="club"
       seo={{ metaTitle: "Club", metaDescription: "St. Louis Bombers" }}
     >
-      <IndexLayout items={navs[2].subMenus}></IndexLayout>
+      <IndexLayout items={clubSubNav}></IndexLayout>
     </Layout>
   );
 };
--- pages/team/index.js
+++ pages/team/index.js
@@ -1,102 +1,16 @@
 import { Box, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
 import useNav from "hooks/useNav";
+import IndexLayout from "../../src/components/IndexLayout";
 import Card from "../../src/common/Card";
 import Layout from "../../src/common/Layout";
 
 const TeamHome = () => {
   const { navs } = useNav();
-  const teamsMenu = navs[3].subMenus;
-  console.log(teamsMenu)
-  // const teams = [
-  //   {
-  //     displayName: "Division 1",
-  //     url: "d1",
-  //     bg: "/static/d1_team.jpeg",
-  //   },
-  //   {
-  //     displayName: "Division 2",
-  //     url: "d2",
-  //     bg: "/static/d3TeamPhoto.JPG",
-  //   },
-  //   {
-  //     displayName: "Coaches & Staff",
-  //     url: "coaches-and-staff",
-  //     bg: "/static/coach_pic1.jpeg",
-  //   },
-  //   {
-  //     displayName: "Legends",
-  //     url: "legends",
-  //     bg: "/static/jk_legends_pic.jpeg",
-  //   },
-  // ];
+  const teamSubMenu = navs[3].subMenus;
+
   return (
     <Layout seo={{ metaTitle: "Team" }} header="Players and Staff">
-      <SimpleGrid
-        h="100%"
-        columns={{ sm: 1, md: 2, lg: 3 }}
-        spacing={[0, 0, 5, 10]}
-        m={[0, 0, 8, 16]}
-      >
-        {teamsMenu.map((team) => {
-          return (
-            <Card
-              key={`${team.slug}`}
-              radius="8px"
-              id="team-card"
-              as={`/team/${team?.slug}`}
-              link={"/team/[division]"}
-              styles={{
-                minHeight: "370px",
-                height: "auto",
-                maxWidth: "300px",
-                display: "flex",
-                justifyContent: "space-around",
-                cursor: "pointer",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                backgroundColor: "gray",
-              }}
-              bg={team.bg}
-              border="1px solid #e2e2e2"
-            >
-              <Box
-                style={{
-                  background: `linear-gradient(
-            185deg,rgba(0,0,0,0) 20%,rgba(0,0,0,.9) 80%)`,
-                  height: "70%",
-                  zIndex: 98,
-                  width: "100%",
-                  position: "absolute",
-                  overflow: "hidden",
-                  bottom: 0,
-                  verticalAlign: "baseline",
-                  display: "flex",
-                }}
-              >
-                <Flex
-                  id="text-content"
-                  p="0 0 16px 20px"
-                  direction="column"
-                  w="100%"
-                  alignSelf="flex-end"
-                >
-                  <Heading
-                    m={0}
-                    color="brand.white"
-                    fontWeight="bold"
-                    textTransform="uppercase"
-                    wordSpacing="6px"
-                    fontSize="2em"
-                  >
-                    {team.name}
-                  </Heading>
-                </Flex>
-              </Box>
-            </Card>
-          );
-        })}
-      </SimpleGrid>
+      <IndexLayout items={teamSubMenu}></IndexLayout>
     </Layout>
   );
 };
