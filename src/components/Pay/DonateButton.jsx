dc18419ed8e7e69d213ca65d3b65d02f15177d1b
edavidson1410 <edavidson1410@gmail.com>
2025-04-11 15:01:22 -0500

    donate page styles

--- src/components/Pay/DonateButton.jsx
+++ src/components/Pay/DonateButton.jsx
@@ -1,51 +1,8 @@
-// import {
-//   Flex,
-//   Input,
-//   InputGroup,
-//   InputLeftAddon,
-//   Radio,
-//   RadioGroup,
-//   SimpleGrid,
-// } from "@chakra-ui/react";
-// import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
-// import { useEffect, useState } from "react";
-
-// const DonateButton = ({ onApprove }) => {
-//   return (
-//     <>
-//       <form
-//         action="https://www.paypal.com/donate"
-//         method="post"
-//         target="_blank"
-//       >
-//         <input type="hidden" name="hosted_button_id" value="5JQ4TPW3RQ9UA" />
-//         <input
-//           type="image"
-//           src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-//           border="0"
-//           name="submit"
-//           title="PayPal - The safer, easier way to pay online!"
-//           alt="Donate with PayPal button"
-//         />
-//         <img
-//           alt=""
-//           border="0"
-//           src="https://www.paypal.com/en_US/i/scr/pixel.gif"
-//           width="1"
-//           height="1"
-//         />
-//       </form>
-//     </>
-//   );
-// };
-
-// export default DonateButton;
 import {
   Flex,
   Heading,
   Text,
   VStack,
-  useColorModeValue,
   Box,
   Link,
   Button,
